f8e5b1efd5a8064a082f71d6c6ffd9da0422ec7b
yuri-misonoo <yuri.panpanda@gmail.com>
2021-11-28 21:30:20 +0900

    styleバインディング

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -8,17 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class HelloComponent implements OnInit {
   title!:string;
   message!:string;
-  data!:string[];
 
   constructor() {}
 
   ngOnInit() {
     this.title = 'Hello-app';
-    this.message = 'date list.';
-    this.data = [
-      '最初の項目です',
-      '2番目の項目です',
-      '最後の項目です'
-    ]
+    this.message = 'Font size';
   }
 }
