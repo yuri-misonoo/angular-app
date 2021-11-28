9a0412e001c358b7247a8351a874a83388f3389b
yuri-misonoo <yuri.panpanda@gmail.com>
2021-11-28 21:49:12 +0900

    checkbox

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -8,11 +8,24 @@ import { Component, OnInit } from '@angular/core';
 export class HelloComponent implements OnInit {
   title!:string;
   message!:string;
+  nowClass:any;
 
   constructor() {}
 
   ngOnInit() {
     this.title = 'Hello-app';
-    this.message = 'Font size';
+    this.message = 'false false false';
+    this.nowClass = {
+      'thin':false,
+      'large':false,
+      'frame':false
+    };
+  }
+
+  check(c1:any, c2:any, c3:any) {
+    this.nowClass.thin = c1;
+    this.nowClass.large = c2;
+    this.nowClass.frame = c3;
+    this.message = c1 + ',' + c2 + ',' + c3;
   }
 }
