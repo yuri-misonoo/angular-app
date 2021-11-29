826d16ad038249b0fb9369d0107622c214aabea1
yuri-misonoo <yuri.panpanda@gmail.com>
2021-11-29 14:58:46 +0900

    ngModel

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -8,24 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class HelloComponent implements OnInit {
   title!:string;
   message!:string;
-  nowStyle:any;
+  text1!:string;
 
   constructor() {}
 
   ngOnInit() {
     this.title = 'Hello-app';
     this.message = 'false false false';
-    this.nowStyle = {
-      'border-style':'',
-      'border-width':'',
-      'border-color':''
-    };
-  }
-
-  check(in1:any, in2:any, in3:any) {
-    this.nowStyle['border-style'] = in1;
-    this.nowStyle['border-width'] = in2 + "px";
-    this.nowStyle['border-color'] = in3;
-    this.message = JSON.stringify(this.nowStyle);
+    this.text1 = '';
   }
 }
