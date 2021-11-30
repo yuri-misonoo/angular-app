289257a27d759cf13ba8a92ded07ae92373f5251
yuri-misonoo <yuri.panpanda@gmail.com>
2021-11-30 16:06:31 +0900

    エラーメッセージの指定　※エラーでる

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,6 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
+function alpha(c:FormControl) {
+  let REGPATTERN = /^[a-zA-Z]+$/;
+  if (REGPATTERN.test(c.value)) {
+    return null;
+  } else {
+    return { alpha:{valid: false }};
+  };
+}
+
+function even(c: FormControl) {
+  return c.value % 2 == 0 ? null : {even:{valid:false}};
+}
+
 @Component({
   selector: 'app-hello',
   templateUrl: './hello.component.html',
@@ -11,15 +24,15 @@ export class HelloComponent implements OnInit {
   message!:string;
   myControl!:FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  constructor() {}
 
   ngOnInit() {
     this.title = 'Hello-app';
     this.message = 'FormControlを使う';
-    this.myControl = this.fb.group({
-      name: ['', [Validators.required]],
-      mail: ['', [Validators.email]],
-      age: [0, [Validators.min(1), Validators.max(150)]]
+    this.myControl = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      mail: new FormControl('', [Validators.email]),
+      age: new FormControl(0, [Validators.min(1), Validators.max(150), even])
     });
   }
 
